cd9da05b696027df0eea115f6e169051132ce755
Elton Santos <eltonaxl@hotmail.com>
2024-09-27 06:17:37 -0300

    Change style and change task statistics to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_available_hours
+  before_action :set_task_statistics
 
   protected
 
@@ -14,6 +15,16 @@ class ApplicationController < ActionController::Base
     commitments = current_user.commitments.includes(:tasks)
     total_hours_used = commitments.flat_map(&:tasks).pluck(:hours).sum || 0
     @available_hours = current_user.hours_per_week - total_hours_used
+  end
+
+  def set_task_statistics
+    @commitments = current_user.commitments.includes(:tasks)
+
+    @total_tasks = @commitments.flat_map(&:tasks).size
+    @in_progress = @commitments.flat_map(&:tasks).select { |task| task.in_progress? }.size
+    @completed = @commitments.flat_map(&:tasks).select { |task| task.completed? }.size
+    @archived = @commitments.flat_map(&:tasks).select { |task| task.archived? }.size
+    @progress_percentage = @total_tasks.zero? ? 0 : (@completed.to_f / @total_tasks * 100).round(2)
   end
 
   def configure_permitted_parameters
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,5 @@ class HomeController < ApplicationController
   
   def index
     @tasks = Task.all.order(:id)
-    @commitments = current_user.commitments.includes(:tasks)
-    
-    @total_tasks = @commitments.flat_map(&:tasks).size
-    @in_progress = @commitments.flat_map(&:tasks).select { |task| task.in_progress? }.size
-    @completed = @commitments.flat_map(&:tasks).select { |task| task.completed? }.size
-    @archived = @commitments.flat_map(&:tasks).select { |task| task.archived? }.size
-    @progress_percentage = @total_tasks.zero? ? 0 : (@completed.to_f / @total_tasks * 100).round(2)
   end
 end
