f0c1eb77b70e4b4f4d7dfe81280f4ac75b58f213
Elton Santos <eltonaxl@hotmail.com>
2025-01-20 00:31:24 -0300

    Change timezone utc to gmt-3

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,10 @@ module Meuscompromissos
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
+    
+    config.time_zone = 'Brasilia'
+    config.active_record.default_timezone = :local 
+
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     # config.i18n.default_locale = :'pt-BR'
